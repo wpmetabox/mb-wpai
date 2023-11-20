9e136d3347cf3cd3a251ab34d3bfd219a35fb5ce
Tan Nguyen <hi@tan.ng>
2023-11-20 09:09:28 +0700

    add test for group & repeatable fields

--- src/fields/FieldFactory.php
+++ src/fields/FieldFactory.php
@@ -2,13 +2,13 @@
 namespace wpai_meta_box_add_on\fields;
 
 final class FieldFactory {
-    
+
 	public static function create( $fieldData, $post, $fieldName = "", $fieldParent = false ): Field {
 		$field_name = str_replace( " ", "", ucwords( str_replace( "_", " ", $fieldData['type'] ) ) );
 		$field_class = __NAMESPACE__ . '\\mb\\' . $field_name;
 
 		if ( ! class_exists( $field_class ) ) {
-			$field_class = __NAMESPACE__ . '\\acf\\Text';
+			$field_class = __NAMESPACE__ . '\\mb\\Text';
 		}
 
 		return new $field_class( $fieldData, $post, $fieldName, $fieldParent );
