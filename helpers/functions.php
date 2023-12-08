1eb7cd56a1ec1e6b7218c90655a128e12d17d700
Tan Nguyen <hi@tan.ng>
2023-12-08 14:19:33 +0700

    refactor: remove unused functions

--- helpers/functions.php
+++ helpers/functions.php
@@ -5,32 +5,3 @@ function pmai_get_meta_box_by_slug( string $slug ): ?RW_Meta_Box {
 
 	return $meta_box_registry->get( $slug );
 }
-
-function pmai_simplify_meta_box( RW_Meta_Box $meta_box ): RW_Meta_Box {
-	$meta_box->meta_box['fields'] = pmai_simplify_fields( $meta_box->meta_box['fields'] );
-
-	return $meta_box;
-}
-
-function pmai_simplify_fields( array $fields, $parent = null ): array {
-	return array_map( function ( $field ) use ( $parent ) {
-		$field_name = $parent ? "{$parent['name']}[{$field['name']}]" : $field['name'];
-
-		if (isset($field['fields'])) {
-			$field['fields'] = pmai_simplify_fields($field['fields'], $field);
-		}
-
-		$field = array_merge( $field, [
-			'name' => 'fields[' . $field_name . ']',
-			'id' => 'fields[' . $field_name . ']',
-			'autocomplete' => false,
-			'data_list'     => false,
-			'readonly'      => false,
-			'disabled'      => false,
-			'multiple'      => false,
-			'placeholder'   => '',
-		] );
-
-		return $field;
-	}, $fields );
-}
